715c44901f4ad3db140547f8d0f332632d7229e6
linkaicheng <1063002901@qq.com>
2016-10-29 21:56:07 +0800

    同步

--- my12306_v1/src/com/user/service/UserService.java
+++ my12306_v1/src/com/user/service/UserService.java
@@ -28,27 +28,27 @@ public class UserService {
 
 	}
 
-//	/**
-//	 * 增加用户
-//	 * 
-//	 * @param user
-//	 */
-//	public void addUser(User user) {
-//		Connection conn = null;
-//		try {
-//			conn = DBUtil.getConnection();
-//			UserDao userDao = new UserDaoImpl(conn);
-//			DBUtil.beginTransaction(conn);
-//			userDao.save(user);
-//			DBUtil.commit(conn);
-//		} catch (SQLException e) {
-//			// TODO: handle exception
-//			DBUtil.rollback(conn);
-//			e.printStackTrace();
-//		} finally {
-//			DBUtil.release(conn);
-//		}
-//	}
+	// /**
+	// * 增加用户
+	// *
+	// * @param user
+	// */
+	// public void addUser(User user) {
+	// Connection conn = null;
+	// try {
+	// conn = DBUtil.getConnection();
+	// UserDao userDao = new UserDaoImpl(conn);
+	// DBUtil.beginTransaction(conn);
+	// userDao.save(user);
+	// DBUtil.commit(conn);
+	// } catch (SQLException e) {
+	// // TODO: handle exception
+	// DBUtil.rollback(conn);
+	// e.printStackTrace();
+	// } finally {
+	// DBUtil.release(conn);
+	// }
+	// }
 	/**
 	 * 增加用户
 	 * 
@@ -57,15 +57,16 @@ public class UserService {
 	 */
 	public int addUser(User user) {
 		UserDaoImp userDao = new UserDaoImp();
-		int row=0;
+		int row = 0;
 		try {
-			 row=userDao.save(user);
+			row = userDao.save(user);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return row;
 	}
+
 	public User findUser(User one) {
 		User user = null;
 		Connection conn = null;
@@ -135,20 +136,21 @@ public class UserService {
 
 	/**
 	 * 获取指定页用户信息列表，通过分页SQL语句实现
+	 * 
 	 * @param pageSize，每页显示信息条数
 	 * @param rowNum，需要获取的页数
 	 * @param one，需要获取的页数
 	 * @return 用户信息列表，List[User]，若无满足条件则列表为空
 	 * @throws SQLException
 	 */
-	public List<User> getUserList(int pageSize, int rowNum, User one){
+	public List<User> getUserList(int pageSize, int rowNum, User one) {
 		Connection conn = null;
 		List<User> res = null;
 		try {
 			conn = DBUtil.getConnection();
 			UserDao userDao = new UserDaoImpl(conn);
 			DBUtil.beginTransaction(conn);
-			res = userDao.getUserList(pageSize,rowNum,one);
+			res = userDao.getUserList(pageSize, rowNum, one);
 			DBUtil.commit(conn);
 		} catch (SQLException e) {
 			DBUtil.rollback(conn);
@@ -156,37 +158,40 @@ public class UserService {
 		} finally {
 			DBUtil.release(conn);
 		}
-		
+
 		return res;
 	}
 
 	/**
 	 * 获取用户列表最大页数
+	 * 
 	 * @param pageSize，每页显示信息条数
 	 * @return 列表最大页数
 	 * @throws SQLException
 	 */
-	public int getUserListPageCount(int pageSize, User one) throws SQLException{
+	public int getUserListPageCount(int pageSize, User one) throws SQLException {
 		Connection conn = null;
 		int res = 0;
 		try {
 			conn = DBUtil.getConnection();
 			UserDao userDao = new UserDaoImpl(conn);
 			DBUtil.beginTransaction(conn);
-			res = userDao.getUserListPageCount(pageSize,one);
+			res = userDao.getUserListPageCount(pageSize, one);
 			DBUtil.commit(conn);
 		} finally {
 			DBUtil.release(conn);
 		}
-		
+
 		return res;
 	}
+
 	/**
 	 * 获取用户列表最大行数
+	 * 
 	 * @return 列表最大行数
 	 * @throws SQLException
 	 */
-	public int getUserListRowCount(User one){
+	public int getUserListRowCount(User one) {
 		Connection conn = null;
 		int res = 0;
 		try {
@@ -201,7 +206,7 @@ public class UserService {
 		} finally {
 			DBUtil.release(conn);
 		}
-		
+
 		return res;
 	}
 }
